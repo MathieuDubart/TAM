a3354210cc4b97b6611902e715bf8d9ad62aacdb
MathieuDubart <mathieu.dubart@icloud.com>
2024-03-14 08:40:04 +0100

    Initial Commit

---
+++ TAM Project/TAM_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TAM_ProjectApp.swift
+//  TAM Project
+//
+//  Created by Mathieu DUBART on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TAM_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
